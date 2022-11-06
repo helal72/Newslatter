4fb73963f5843568912bceac7521739ef5c18ef2
Md.Helal Hossen <helal.cse.pust72@gmail.com>
2022-11-06 23:14:08 +0600

    30 Cmplited

--- src/Componants/Navbar.js
+++ src/Componants/Navbar.js
@@ -3,7 +3,7 @@ import React, { Component } from 'react'
 export class Navbar extends Component {
   render() {
     return (
-        <nav className="navbar navbar-expand-lg bg-light">
+        <nav className="navbar navbar-expand-lg navbar-dark bg-dark">
         <div className="container-fluid">
           <a className="navbar-brand" href="/">Navbar</a>
           <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
@@ -17,6 +17,13 @@ export class Navbar extends Component {
               <li className="nav-item">
                 <a className="nav-link" href="/about">About</a>
               </li>
+                <li className="nav-item"><a className="nav-link" href="/about">business</a></li>
+                <li className="nav-item"><a className="nav-link" href="/about">entertainment</a></li>
+                <li className="nav-item"><a className="nav-link" href="/about">general</a></li>
+                <li className="nav-item"><a className="nav-link" href="/about">health</a></li>
+                <li className="nav-item"><a className="nav-link" href="/about">science</a></li>
+                <li className="nav-item"><a className="nav-link" href="/about">sports</a></li>
+                <li className="nav-item"><a className="nav-link" href="/about">technology</a></li>
             </ul>
           </div>
         </div>
--- src/Componants/NewsItem.js
+++ src/Componants/NewsItem.js
@@ -3,15 +3,15 @@ import React, { Component } from 'react'
 export class NewsItem extends Component {
    
   render() {
-    let {title,discription,imageUrl}= this.props;
+    let {title,discription,imageUrl,newsUrl}= this.props;
     return (
       <div className='my-3'>
-        <div className="card" style={{width: "18rem"}}>
-            <img src={imageUrl} className="card-img-top" alt="..."/>
+        <div className="card">
+            <img src={imageUrl?imageUrl:"https://images.prothomalo.com/prothomalo-bangla%2F2022-10%2F23db4ea7-bce2-4b82-8d93-7c8581af6bd6%2FPA_17082020_P_11_1.jpg?rect=37%2C0%2C1293%2C679&w=1200&ar=40%3A21&auto=format%2Ccompress&ogImage=true&mode=crop&overlay=https%3A%2F%2Fimages.prothomalo.com%2Fprothomalo-bangla%2F2022-01%2F18c58c05-9e66-46f3-8946-4437460f8f90%2FBanner_7814X143.jpg&overlay_position=bottom&overlay_width_pct=1"} className="card-img-top" alt="..."/>
             <div className="card-body">
                 <h5 className="card-title">{title}</h5>
-                <p className="card-text">{discription}</p>
-                <a href="/nwesDetails" className="btn btn-sm btn-primary">Read more</a>
+                <p className="card-text">{discription}...</p>
+                <a href={newsUrl} className="btn btn-sm btn-primary">Read more</a>
             </div>
         </div>
       </div>
